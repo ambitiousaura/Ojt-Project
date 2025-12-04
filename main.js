b978989c0fffc0656f2acdef4f90fd34bc9d20e0
ambitiousaura <vanshsaini.29.04@gmail.com>
2025-12-04 14:30:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+// Entry point module.
+// Waits for the DOM to be ready, then starts the app.
+
+import { initApp } from "./uii.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+  initApp();
+});
 
